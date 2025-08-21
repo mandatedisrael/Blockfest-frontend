3c804f702e18ecb6a0edc5aeb42c890f84f52ba6
Henry Agu <aguhenrychuks@gmail.com>
2025-08-21 01:12:02 +0100

    feat: footer for desktop

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -2,44 +2,46 @@ import Image from "next/image";
 import React from "react";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import { FaInstagram, FaLinkedin, FaXTwitter, FaYoutube } from "react-icons/fa6";
 
 interface FooterMenu {
   path: string;
-  title: string;
+  title?: string;
+  icon?: React.ReactNode; // change to React.ReactNode instead of string
 }
 
-const footerMenu: FooterMenu[] = [
+const socialIcons: FooterMenu[] = [
   {
     path: "/",
-    title: "Agenda",
+    icon: <FaXTwitter size={24} />,
   },
   {
     path: "/",
-    title: "About",
+    icon: <FaInstagram size={24} />,
   },
   {
     path: "/",
-    title: "Sponsors",
+    icon: <FaYoutube size={24} />,
   },
   {
     path: "/",
-    title: "Blogs",
+    icon: <FaLinkedin size={24} />,
   },
+];
 
-  {
-    path: "/",
-    title: "Speakers",
-  },
-  {
-    path: "/",
-    title: "Contacts",
-  },
+const footerMenu: FooterMenu[] = [
+  { path: "/", title: "Agenda" },
+  { path: "/", title: "About" },
+  { path: "/", title: "Sponsors" },
+  { path: "/", title: "Blogs" },
+  { path: "/", title: "Speakers" },
+  { path: "/", title: "Contacts" },
 ];
 
 const Footer = () => {
   return (
     <footer className="bg-black p-[70px] flex items-end justify-between">
-      <div className="">
+      <div>
         <Image
           src="/images/footer-logo.svg"
           alt="Logo"
@@ -48,11 +50,13 @@ const Footer = () => {
           className="w-[150px] h-[49px] aspect-[150/49]"
         />
       </div>
+
       <div className="flex items-end gap-x-12.5">
+        {/* Navigation Menu */}
         <nav className="grid grid-cols-3 gap-x-12.5 gap-y-6">
           {footerMenu.map((item) => (
             <Link
-              key={item.path}
+              key={item.title}
               href={item.path}
               className="text-2xl font-medium cursor-pointer text-[#A4A4A4] hover:text-white transition-colors duration-300 ease-in-out"
             >
@@ -60,9 +64,24 @@ const Footer = () => {
             </Link>
           ))}
         </nav>
-        <div>1</div>
+
+        {/* Social Icons */}
+        <div className="flex items-center gap-x-3">
+          {socialIcons.map((item, index) =>
+            item.icon ? (
+              <Link
+                href={item.path}
+                key={index}
+                className="cursor-pointer text-[#A4A4A4] hover:text-white transition-colors duration-300 ease-in-out"
+              >
+                {item.icon}
+              </Link>
+            ) : null
+          )}
+        </div>
       </div>
-      <div className="">
+
+      <div>
         <Button
           type="button"
           variant="ghost"
