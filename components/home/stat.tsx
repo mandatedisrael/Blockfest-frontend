c0422a13f6425edb8a20fa9229b8b31caf413071
Henry Agu <aguhenrychuks@gmail.com>
2025-08-21 10:23:29 +0100

    style: stat responsive for mobile

--- components/home/stat.tsx
+++ components/home/stat.tsx
@@ -1,39 +1,25 @@
-import React from 'react';
+import React from "react";
+
+const stats = [
+  { value: "4,000+", label: "Attendees" },
+  { value: "20+", label: "Exhibitors" },
+  { value: "20+", label: "Speakers" },
+  { value: "54+", label: "Countries" },
+];
+
 export function StatsSection() {
   return (
-    <section className="flex items-center justify-evenly  text-center py-[35px] bg-gradient-to-r from-[#1B64E4] to-[#031940] text-white">
-      <div className="flex flex-col items-center space-y-2">
-        <p className="font-medium text-[30px] leading-[17.61px] tracking-[-5%]">
-          4,000+
-        </p>
-        <p className="font-normal text-[19.5px] leading-[17.61px]">
-          Attendees
-        </p>
-      </div>
-      <div className="flex flex-col items-center space-y-2">
-        <p className="font-medium text-[30px] leading-[17.61px] tracking-[-5%]">
-          20+
-        </p>
-        <p className="font-normal text-[19.5px] leading-[17.61px]">
-          Exhibitors
-        </p>
-      </div>
-      <div className="flex flex-col items-center space-y-2">
-        <p className="font-medium text-[30px] leading-[17.61px] tracking-[-5%]">
-          20+
-        </p>
-        <p className="font-normal text-[19.5px] leading-[17.61px]">
-          Speakers
-        </p>
-      </div>
-      <div className="flex flex-col items-center space-y-2">
-        <p className="font-medium text-[30px] leading-[17.61px] tracking-[-5%]">
-          54+
-        </p>
-        <p className="font-normal text-[19.5px] leading-[17.61px]">
-          Countries
-        </p>
-      </div>
+    <section className="flex items-center justify-between xl:justify-evenly px-5 text-center py-[35px] bg-gradient-to-r from-[#1B64E4] to-[#031940] text-white">
+      {stats.map((stat, index) => (
+        <div key={index} className="flex flex-col items-center xl:space-y-2">
+          <p className="font-medium text-xl xl:text-[30px] xl:leading-[17.61px] tracking-[-5%]">
+            {stat.value}
+          </p>
+          <p className="font-normal text-base xl:text-[19.5px] xl:leading-[17.61px]">
+            {stat.label}
+          </p>
+        </div>
+      ))}
     </section>
   );
 }
